181f0e81b66c0d1c43ca9abc667f9b67773710d4
Rahul Ranjan Pandey <62777073+rahulranjanpandey@users.noreply.github.com>
2021-08-02 12:08:41 +0530

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+
+checkButton.addEventListener("click", function validateBillAndCashAmount);
+
+checkButton.addEventListener("click", function validateBIllAndCashAmount(){
+    if(billAmount.value > 0)
+}
+)
