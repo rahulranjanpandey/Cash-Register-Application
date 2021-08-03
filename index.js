6d0d401b05eab74d5db39394edde697c65de34c5
Rahul Ranjan Pandey <62777073+rahulranjanpandey@users.noreply.github.com>
2021-08-04 00:06:54 +0530

    commit

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
 
-checkButton.addEventListener("click", function validateBillAndCashAmount);
+checkButton.addEventListener("click", function validateBillAndCashAmount(){
+    if(billAmount.value > 0) {
+        message.style.display = "none"; 
+        else  {
+            message.style.dosplay = "block";
+            message.innerText = "The bill amount should be greater than 0";
+        }
 
-checkButton.addEventListener("click", function validateBIllAndCashAmount(){
-    if(billAmount.value > 0)
+    }
 }
-)
+);
