adeccf5754b93e72bb1458b5ff2831105c1a3b27
Rahul Ranjan Pandey <62777073+rahulranjanpandey@users.noreply.github.com>
2021-08-05 23:06:50 +0530

    commit

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const message = document.querySelector("#error-message");
 
 checkButton.addEventListener("click", function validateBillAndCashAmount(){
     if(billAmount.value > 0) {
-        message.style.display = "none"; 
-        else  {
-            message.style.dosplay = "block";
-            message.innerText = "The bill amount should be greater than 0";
+        if (cashGiven.value >= billAmount.value) {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+        } else {
+            showMessage("Do you wanna wash plates ?");
         }
-
+    } else {
+        showMessage("Invalid Bill Amount");
     }
+});
+
+function calculateChange(amountToBeReturned) {
+       
 }
-);
+
