e54f0fa2d1ca1a0c444aafd6621d9faf2e23f59a
Rahul Ranjan Pandey <62777073+rahulranjanpandey@users.noreply.github.com>
2021-08-09 11:22:26 +0530

    update

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount(){
+    hideMessage();
     if(billAmount.value > 0) {
         if (cashGiven.value >= billAmount.value) {
             const amountToBeReturned = cashGiven.value - billAmount.value;
@@ -25,7 +26,7 @@ function calculateChange(amountToBeReturned) {
         // no. of notes need for the denomination
         const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
         // amount left after calculating the number of notes needed
-        amountToBeReturned %= availableNotes[i];
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
         noOfNotes[i].innerText = numberOfNotes;
     }
 }
